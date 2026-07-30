1d5b9b0c268a56d690cbb38529ef80f20c07ec07
Nathan <nathan.wolker@escola.pr.gov.br>
2026-07-30 12:30:02 -0300

    .

--- script.js
+++ script.js
@@ -5,18 +5,12 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
+  {
     enunciado: "Pergunta 1",
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-}, 
-{
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
     enunciado: "Pergunta 2",
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-}    
-]
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
