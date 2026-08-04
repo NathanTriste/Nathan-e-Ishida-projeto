a9e0b4daa0589f9569904488cac0d9d73385fdd3
Nathan <nathan.wolker@escola.pr.gov.br>
2026-08-04 11:39:04 -0300

    .

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Assim que acaba a Copa do Mundo 2026, você se depara com o Neymar aposentado.",
+    alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "O Neymar pós aposentado, quer entrar para o Athletico Paranaense, porém precisa de votos pra isso.",
+    alternativas: ["Eu votaria!", "Eu não votaria!"],
   },
   {
-    enunciado: "Pergunta 3",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "O Neymar estando no Athletico Paranaense, se depara com um lançe decisivo para ser campeão da Libertadores.",
+    alternativas: ["Ele chuta no ângulo do gol, e faria o gol.", "Cavaria um pênalti."],
   },
   {
-    enunciado: "Pergunta 4",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Qual foi o clube europeu pelo qual Neymar Jr. conquistou a UEFA Champions League e para qual equipe ele se transferiu na transação mais cara da história do futebol?",
+    alternativas: ["FC Barcelona * Contexto: Neymar conquistou a Liga dos Campeões na temporada 2014/2015, formando o famoso trio de ataque "MSN" ao lado de Lionel Messi e Luis Suárez.", "Paris Saint-Germain (PSG) * Contexto: Em agosto de 2017, o PSG pagou a multa rescisória de 222 milhões de euros para tirá-lo do Barcelona, tornando-a a transferência mais cara da história do futebol mundial."],
   },
   {
-    enunciado: "Pergunta 5",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Quais são os dois maiores artilheiros da história da Seleção Brasileira masculina (em jogos oficiais)?",
+    alternativas: ["Neymar e Pelé.", "Manoel Gomez e Tony Ramos."],
   },
 ];
