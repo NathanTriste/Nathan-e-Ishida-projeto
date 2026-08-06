e67e7a783f7e1d2f277bf28b8465690e77136f3f
Nathan <nathan.wolker@escola.pr.gov.br>
2026-08-06 12:14:48 -0300

    .

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
 mostraPergunta();
